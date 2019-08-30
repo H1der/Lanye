411e66653e60d1c1fd5a9f1b1c0b82cce3c7debd
Hider <724598894@qq.com>
2019-08-30 17:24:34 +0800

    帮助页面完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,89 +1,123 @@
 // pages/index/index.js
+var url = 'https://interface.meiriyiwen.com/article';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      title:'',
-      author:'',
-      content:''
+    title: '',
+    author: '',
+    content: ''
   },
 
   /**
    * 获取今天的文章
    */
-  getTodayArticle:function(e){
+  getTodayArticle: function(e) {
     var that = this;
     wx: wx.request({
-      url: 'https://interface.meiriyiwen.com/article/today?dev=1',
+      url: url + '/today?dev=1',
       method: 'GET',
-      dataType: 'json',
-      responseType: 'text',
-      success: function (res) { 
-       console.log(res);
+      success: function(res) {
+        // console.log(res);
         that.setData({
-          title:res.data.data.title,
+          title: res.data.data.title,
           author: res.data.data.author,
-          content:res.data.data.content
+          content: res.data.data.content
         })
-      }
+      },
+      complete: function(res) {
+        wx.pageScrollTo({
+          scrollTop: 0,
+          duration: 300
+        });
+      },
     })
   },
 
   /**
+   * 随机获取文章
+   */
+  getRandomArticle: function(e) {
+    var that = this;
+    wx: wx.request({
+      url: url + '/random?dev=1',
+      method: 'GET',
+      success: function(res) {
+        // console.log(res)
+        that.setData({
+          title: res.data.data.title,
+          author: res.data.data.author,
+          content: res.data.data.content
+        })
+      },
+      complete: function(res) {
+        wx.stopPullDownRefresh();
+      },
+    })
+  },
+/**
+ * 跳转到帮助页
+ */
+  goHelp: function () {
+    wx.navigateTo({
+      url: "../help/help"
+    });
+  },
+  /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.getTodayArticle();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    this.getRandomArticle();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
